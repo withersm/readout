4b4ddf03bafb8c7e3b899949f4e8289ff46daf31
Cody Roberson <carobers@asu.edu>
2023-07-26 15:01:06 -0700

    modified udp2
    Decided to not seperate process between capture and write. Hopefully
    this is still fine.

--- host/udp2.py
+++ host/udp2.py
@@ -1,11 +1,23 @@
+"""
+Description
+___________
+
+udp2 is the Next iteration of udpcap. The idea here is to facilitate taking data in a multiprocess enviornment
+from multiple channels from multiple RFSOC's in our mKID readout system.
+
+:Authors: Cody
+:Date: 2023-07-26
+:Version: 1.0.0
+"""
 import concurrent.futures
-import multiprocessing as mlpr
 import logging
 import socket
 from dataclasses import dataclass
 import numpy as np
 import time
 import data_handler
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -15,68 +27,39 @@ class Connection:
     port: int = 0000
 
 
-def data_writer(raw_df: data_handler.RawDataFile, queue):
+def __receive_udp(conn: Connection, n_samples: int):
     """
-    data_writer child process. the receive_data parent process will
-    pull down udp packets and pass it off to this process using a shared queue.
-    :param raw_df: raw observation data file
-    :param queue: process queue data
-    :return: None
-    """
-    active = True
+    UDP data receiver process. *This function is intended to be executed from a ProcessPoolExecutor*
 
-    # While active, get data from the queue and place into specified hdf file.
-    while active:
-        queuedata = queue.get()
-        if queuedata is not None:
-            spec_data, i, t = queuedata
-            raw_df.adc_i[:, i] = spec_data[0::2]
-            raw_df.adc_q[:, i] = spec_data[1::2]
-            raw_df.timestamp[0, i] = t
+    Parameters
+    __________
 
-            # hardware counter left out of firmware for the time being.
-            raw_df.timestamp[1, i] = 12345678
-        else:
-            active = False
-            raw_df.fh.flush()
-
-
-def receive_udp(conn: Connection, n_samples: int):
-    """
-    receive_udp child process. The capture function will generate N Connection subprocesses
-    of this function. Here, udp data is captured and passed off to the data_writer dataprocess.
-    This is where the connection is actually bound
-
-    :param conn: Connection
+    :param conn: RFSOC udp connection to obtain data from.
+    :type conn: Connection
     :param n_samples: Number of samples to take
-    :return: None
+    :type n_samples: int
+    :return: Does not return anything
     """
+    log = logger.getChild(__name__)
     # create a socket and bind it to the connection
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(1.0)
 
     host = conn.ip_addr
     port = conn.port
+    raw_df = conn.raw_df
 
     try:
         sock.bind((host, port))
     except socket.timeout:
-        logging.getLogger(__name__).error(
-            f"socket.timeout -> Could not bind to the socket for {host}:{port}"
-        )
+        log.error(f"socket.timeout -> Could not bind to the socket for {host}:{port}")
     except socket.error:
-        logging.getLogger(__name__).error(
-            f"socket.error -> Could not bind to the socket for {host}:{port}"
-        )
+        log.error(f"socket.error -> Could not bind to the socket for {host}:{port}")
 
-    # create an async datawriter process
-    manager = mlpr.Manager()
-    pool = mlpr.Pool(1)
-    queue = manager.Queue()
-    pool.apply_async(data_writer, (conn.raw_df, queue))
-
-    # Lets get to work grabbing data
+    # Take data and add into HDF File
     i = 0
+    log.debug(f"Beginning data taking loop for {conn.ip_addr}:{conn.port}")
+
     while i < n_samples:
         data = sock.recv(8208)
         spec_data = np.frombuffer(data, dtype="<i", offset=0)
@@ -85,21 +68,27 @@ def receive_udp(conn: Connection, n_samples: int):
         if data is None:
             break
 
-        queue.put((spec_data, i, t))
-        i = i + 1
+        raw_df.adc_i[:, i] = spec_data[0::2]
+        raw_df.adc_q[:, i] = spec_data[1::2]
+        raw_df.timestamp[0, i] = t
 
-    pool.close()
+        i = i + 1
 
 
 def capture(connection_list: list, n_samples: int):
     """
     Capture Data. Spawns N connection receive_udp processes for data taking
+
+    Parameters
+    __________
+
     :param connection_list: List of Connections
     :param n_samples: n samples to record.
     :return: None
     """
+    log = logger.getChild(__name__)
     # create subprocesses for each udp connection.
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for con in connection_list:
-            print("spawning process...")
-            executor.submit(receive_udp, con, n_samples)
+            log.debug(f"Spawning receive_udp process for {con.ip_addr}:{con.port}")
+            executor.submit(__receive_udp, con, n_samples)
